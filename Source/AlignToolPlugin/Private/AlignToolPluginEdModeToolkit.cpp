7e31fef56b5ce5670050036a4f63af6c2b91af4b
Robert Gentile <robertngentile@gmail.com>
2016-12-08 05:04:37 -0500

    buttons and allignment seem to be working, might be cases where first selected is not registered properly. needs more testing/polish

--- Source/AlignToolPlugin/Private/AlignToolPluginEdModeToolkit.cpp
+++ Source/AlignToolPlugin/Private/AlignToolPluginEdModeToolkit.cpp
@@ -18,7 +18,7 @@ void FAlignToolPluginEdModeToolkit::Init(const TSharedPtr<IToolkitHost>& InitToo
 		{
 			return GEditor->GetSelectedActors()->Num() != 0;
 		}
-
+		/*
 		static FReply OnButtonClick(FVector InOffset)
 		{
 			USelection* SelectedActors = GEditor->GetSelectedActors();
@@ -42,14 +42,68 @@ void FAlignToolPluginEdModeToolkit::Init(const TSharedPtr<IToolkitHost>& InitToo
 			GEditor->EndTransaction();
 
 			return FReply::Handled();
-		}
+		}*/
 
+
+		static FReply OnButtonClick(FVector AllignAxisBools, FVector AllignRotationBool )
+		{
+			USelection* SelectedActors = GEditor->GetSelectedActors();
+			AActor* baseActor = Cast<AActor>(SelectedActors->GetSelectedObject(0)); // set first selected as base of allignment
+			bool firstPass = true;
+			// Let editor know that we're about to do something that we want to undo/redo
+			GEditor->BeginTransaction(LOCTEXT("MoveActorsTransactionName", "MoveActors"));
+
+			// For each selected actor
+			for (FSelectionIterator Iter(*SelectedActors); Iter; ++Iter)
+			{
+				if (firstPass)
+				{
+					//baseActor = Cast<AActor>(*Iter);
+					firstPass = false;
+					
+				}
+				else if (AActor* LevelActor = Cast<AActor>(*Iter))
+				{
+					LevelActor->Modify();
+
+					FVector originPosBool = (FVector(1, 1, 1) - AllignAxisBools).GetAbs();// returns the non transformed point bool
+					
+					FVector newPos = (baseActor->GetActorLocation() * AllignAxisBools) + (LevelActor->GetActorLocation() * originPosBool);
+					FRotator newRot = LevelActor->GetActorRotation();
+					
+					if (AllignRotationBool.X)
+					{newRot.Pitch = baseActor->GetActorRotation().Pitch;}
+					else if (AllignRotationBool.Y)
+					{newRot.Roll = baseActor->GetActorRotation().Roll;}
+					else if (AllignRotationBool.Z)
+					{newRot.Yaw = baseActor->GetActorRotation().Yaw;}
+
+
+					LevelActor->TeleportTo(newPos, newRot);
+
+				}
+				
+			}
+
+			// We're done moving actors so close transaction
+			GEditor->EndTransaction();
+
+			return FReply::Handled();
+		}
+		/*
 		static TSharedRef<SWidget> MakeButton(FText InLabel, const FVector InOffset)
 		{
 			return SNew(SButton)
 				.Text(InLabel)
 				.OnClicked_Static(&Locals::OnButtonClick, InOffset);
+		}*/
+		static TSharedRef<SWidget> MakeButton(FText InLabel, const FVector AllignAxisBools, const FVector AllignRotationBool)
+		{
+			return SNew(SButton)
+				.Text(InLabel)
+				.OnClicked_Static(&Locals::OnButtonClick, AllignAxisBools, AllignRotationBool);
 		}
+
 	};
 
 	const float Factor = 256.0f;
@@ -67,35 +121,49 @@ void FAlignToolPluginEdModeToolkit::Init(const TSharedPtr<IToolkitHost>& InitToo
 			[
 				SNew(STextBlock)
 				.AutoWrapText(true)
-				.Text(LOCTEXT("HelperLabel", "Select some actors and move them around using buttons below"))
+				.Text(LOCTEXT("HelperLabel", "AWWWW YEE BOSS CHECK THIS SHIT OUT"))
 			]
 			+ SVerticalBox::Slot()
 				.HAlign(HAlign_Center)
 				.AutoHeight()
 				[
-					Locals::MakeButton(LOCTEXT("UpButtonLabel", "Up"), FVector(0, 0, Factor))
+					Locals::MakeButton(LOCTEXT("TestButtonLabel", "Test"), FVector(0, 0, 0), FVector(0,0,0))
 				]
 			+ SVerticalBox::Slot()
 				.HAlign(HAlign_Center)
 				.AutoHeight()
 				[
-					SNew(SHorizontalBox)
-					+ SHorizontalBox::Slot()
-					.AutoWidth()
-					[
-						Locals::MakeButton(LOCTEXT("LeftButtonLabel", "Left"), FVector(0, -Factor, 0))
-					]
-					+ SHorizontalBox::Slot()
-						.AutoWidth()
-						[
-							Locals::MakeButton(LOCTEXT("RightButtonLabel", "Right"), FVector(0, Factor, 0))
-						]
+					Locals::MakeButton(LOCTEXT("XAlignButtonLabel", "Align on the X Axis"), FVector(0, 1, 1), FVector(0, 0, 0))
 				]
 			+ SVerticalBox::Slot()
 				.HAlign(HAlign_Center)
 				.AutoHeight()
 				[
-					Locals::MakeButton(LOCTEXT("DownButtonLabel", "Down"), FVector(0, 0, -Factor))
+					Locals::MakeButton(LOCTEXT("YAlignButtonLabel", "Align on the Y Axis"), FVector(1, 0, 1), FVector(0, 0, 0))
+				]
+			+ SVerticalBox::Slot()
+				.HAlign(HAlign_Center)
+				.AutoHeight()
+				[
+					Locals::MakeButton(LOCTEXT("ZAlignButtonLabel", "Align on the Z Axis"), FVector(1, 1, 0), FVector(0, 0, 0))
+				]
+			+ SVerticalBox::Slot()
+				.HAlign(HAlign_Center)
+				.AutoHeight()
+				[
+					Locals::MakeButton(LOCTEXT("XRotateButtonLabel", "Rotate on the X Axis"), FVector(0, 0, 0), FVector(1, 0, 0))
+				]
+			+ SVerticalBox::Slot()
+				.HAlign(HAlign_Center)
+				.AutoHeight()
+				[
+					Locals::MakeButton(LOCTEXT("YRotateButtonLabel", "Rotate on the Y Axis"), FVector(0, 0, 0), FVector(0, 1, 0))
+				]
+			+ SVerticalBox::Slot()
+				.HAlign(HAlign_Center)
+				.AutoHeight()
+				[
+					Locals::MakeButton(LOCTEXT("ZRotateButtonLabel", "Rotate on the Z Axis"), FVector(0, 0, 0), FVector(0, 0, 1))
 				]
 
 		];
